c08c014c9c1554e36dff187ff0e5479de6a3fe74
Andrew Kim <adkim5494@gmail.com>
2015-04-25 01:04:48 -0700

    Boundary Conditions made
    Prevents player from moving outside of screen

--- myrect.cpp
+++ myrect.cpp
@@ -6,21 +6,29 @@
 void MyRect::keyPressEvent(QKeyEvent *event){
     //moving the rectangle
 
-
+    //checking if player is outside of scene boundary
     if (event->key() == Qt::Key_Left){
+        if (pos().x() > 0){
         setPos(x()-10,y());
+        }
     }
 
     else if (event->key() == Qt::Key_Right){
+        if (pos().x()+100 < 800){
         setPos(x()+10,y());
+        }
     }
 
     else if (event->key() == Qt::Key_Up){
+        if (pos().y() > 0){
         setPos(x(),y()-10); //y axis is oriented downward
+        }
     }
 
     else if (event->key() == Qt::Key_Down){
+        if (pos().y()+100 < 600){
         setPos(x(),y()+10);
+        }
     }
 
     //shooting bullets
