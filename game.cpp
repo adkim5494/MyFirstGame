8d480089a64b7e9db684dd90a81a95f5c4328d28
Andrew Kim <adkim5494@gmail.com>
2015-04-25 03:05:55 -0700

    Update Game.cpp
    generalized player to be in middle of screen

--- game.cpp
+++ game.cpp
@@ -12,14 +12,16 @@ Game::Game(QWidget *parent){
     // make the newly created scene the scene to visualize (since Game is a QGraphicsView Widget,
     // it can be used to visualize scenes)
     setScene(scene);
-    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setFixedSize(800,600);
+
+    //add a view
+    QGraphicsView* view = new QGraphicsView(scene);
+    view->setFixedSize(800,600);//fix the view size
 
     // create the player
     player = new Player();
     player->setRect(0,0,100,100); // change the rect from 0x0 (default) to 100x100 pixels
-    player->setPos(400,500); // TODO generalize to always be in the middle bottom of screen
+    player->setPos(view->width()/2-50,view->height() - player->rect().height());
     // make the player focusable and set it to be the current focus
     player->setFlag(QGraphicsItem::ItemIsFocusable);
     player->setFocus();
@@ -36,7 +38,7 @@ Game::Game(QWidget *parent){
     // spawn enemies
     QTimer * timer = new QTimer();
     QObject::connect(timer,SIGNAL(timeout()),player,SLOT(spawn()));
-    timer->start(2000);
+    timer->start(2000);//enemy spawns every 2000 miliseconds
 
     show();
 }
